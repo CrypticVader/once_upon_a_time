2bcd9f2c2464ebda534fa52dd69784aeadb2447b
Cryptic_Vader <akhilbnair101@gmail.com>
2023-06-10 20:32:53 +0530

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,53 @@
+import 'dart:ui';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle(
+      systemNavigationBarColor: Colors.black.withOpacity(0.002),
+    ),
+  );
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Once upon a time',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        fontFamily: 'JosefinSans',
+        fontFamilyFallback: const ['Roboto'],
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.deepOrange,
+          brightness: Brightness.light,
+        ),
+        splashFactory:
+            (kIsWeb) ? InkRipple.splashFactory : InkSparkle.splashFactory,
         useMaterial3: true,
       ),
+      darkTheme: ThemeData(
+        fontFamily: 'JosefinSans',
+        fontFamilyFallback: const ['Roboto'],
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.deepOrange,
+          brightness: Brightness.dark,
+        ),
+        splashFactory:
+            (kIsWeb) ? InkRipple.splashFactory : InkSparkle.splashFactory,
+        useMaterial3: true,
+      ),
+      themeMode: ThemeMode.system,
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
@@ -39,15 +56,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -55,71 +63,111 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+      backgroundColor: Theme.of(context).colorScheme.surfaceVariant,
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Stack(
+          fit: StackFit.expand,
+          children: [
+            Image.asset('assets/images/home_bg.png'),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(
+                  height: 64,
+                ),
+                const Text(
+                  'Once',
+                  style: TextStyle(
+                    fontSize: 40,
+                    fontVariations: <FontVariation>[FontVariation('wght', 700)],
+                  ),
+                ),
+                const Text(
+                  'upon',
+                  style: TextStyle(
+                      fontSize: 40,
+                      fontVariations: <FontVariation>[
+                        FontVariation('wght', 700)
+                      ]),
+                ),
+                const Text(
+                  'a time...',
+                  style: TextStyle(
+                      fontSize: 40,
+                      fontVariations: <FontVariation>[
+                        FontVariation('wght', 700)
+                      ]),
+                ),
+                const Spacer(
+                  flex: 1,
+                ),
+                const Text(
+                  'Tell us who you are',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontVariations: <FontVariation>[FontVariation('wght', 400)],
+                  ),
+                ),
+                const SizedBox(
+                  height: 8,
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    contentPadding: const EdgeInsets.all(16),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(24),
+                      borderSide: BorderSide(
+                        width: 2,
+                        color: Theme.of(context).colorScheme.primary,
+                      ),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(16),
+                      borderSide: BorderSide.none,
+                    ),
+                    fillColor:
+                        Theme.of(context).colorScheme.secondary.withAlpha(100),
+                    filled: true,
+                    prefixIconColor: Theme.of(context).colorScheme.primary,
+                    prefixIcon: const Icon(
+                      Icons.person_rounded,
+                    ),
+                    hintText: 'How do you go by...',
+                    hintStyle: const TextStyle(
+                      fontVariations: <FontVariation>[
+                        FontVariation('wght', 700)
+                      ],
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 8,
+                ),
+                FilledButton.icon(
+                  onPressed: () {},
+                  label: const Text(
+                    'Start your journey',
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontVariations: <FontVariation>[
+                        FontVariation('wght', 400)
+                      ],
+                    ),
+                  ),
+                  icon: const Icon(Icons.book_rounded),
+                ),
+                const SizedBox(
+                  height: 32,
+                ),
+              ],
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
